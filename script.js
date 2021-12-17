fb175d42d03e60b91407a59e13c3819029431bc9
Chris Jimenez <chrisjimenez8118@gmail.com>
2021-12-16 19:01:50 -0700

    Updated JS and HTML files to display the correct hours and colors are now appearing in calendar.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 //Display the current date
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-console.log(today)
+console.log(today);
 
-//Display timeblocks for different business hours
 
+var time = document.getElementById("#9")
+var past = document.querySelector(".past")
+var present = document.querySelector(".present")
+var future = document.querySelector(".future")
 //Timeblocks are color coded to represent past, present and future
 
-//Clicking on timeblock will allow the user to type an event in
-
 //Clicking the save button will save the event in local storage
+function trackTime() {
+    if (today > 9) {
+        present.classList.add('present');
+    }
+}
+trackTime();
+//if (today > the time displayed in the calendar, display the middle boxes in gray)
+//else if (today = the time displayed in the claendar, display the middle boxes red)
+//else display the middle boxes green 
+//try adding and ID to each different time and create a variable for each
